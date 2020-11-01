e0b707876bc3a87f34b90449a7ae3c61c0d2ed9b
Marcelo Santos <rocha.mer21@gmail.com>
2020-11-01 20:01:09 -0300

    config iniciais

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fec03b1763ab583155784c8c3ae65aa7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ce83a355-3da8-403e-b458-f1be78577e06'
+};
