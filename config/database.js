27fc7527d7dbde89e7afef0e56b2fceee0325081
Marcelo Santos <rocha.mer21@gmail.com>
2021-03-29 00:55:23 -0300

    tentando

--- config/database.js
+++ config/database.js
@@ -13,9 +13,13 @@ module.exports = ({ env }) => ({
         database: env('DATABASE_NAME', 'blog-strapi'),
         username: env('DATABASE_USERNAME', 'postgres'),
         password: env('DATABASE_PASSWORD', 'senha'),
-        ssl: env.bool('DATABASE_SSL', false),
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
-      options: {},
+      options: {
+        ssl: true,
+      },
     },
   },
 })
