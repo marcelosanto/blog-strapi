85c20e14c7f5201135f2441b36c4bf3a5f150356
Marcelo Santos <rocha.mer21@gmail.com>
2021-12-07 11:29:03 -0300

    fix: lifecycles

--- api/posts/models/posts.js
+++ api/posts/models/posts.js
@@ -8,11 +8,13 @@ const axios = require('axios')
  */
 
 module.exports = {
-  async afterCreate(result, data) {
-    axios.pots('https://api.netlify.com/build_hooks/60ad4ab6fff3141245d55814')
-  },
+  lifecycles: {
+    async afterCreate(result, data) {
+      axios.pots('https://api.netlify.com/build_hooks/60ad4ab6fff3141245d55814')
+    },
 
-  async afterUpdate(result, params, data) {
-    axios.pots('https://api.netlify.com/build_hooks/60ad4ab6fff3141245d55814')
+    async afterUpdate(result, params, data) {
+      axios.pots('https://api.netlify.com/build_hooks/60ad4ab6fff3141245d55814')
+    },
   },
 }
